ad84e10ea1eeb7dd8c9f5c9264af7f54d84c14fe
Git-Gfigueroa2912 <german_figueroa@unitec.edu>
2025-11-14 16:45:25 -0600

    Segundo Commit

--- src/ex1p1_germanfigueroa/Ex1P1_GermanFigueroa.java
+++ src/ex1p1_germanfigueroa/Ex1P1_GermanFigueroa.java
@@ -4,17 +4,80 @@
  */
 package ex1p1_germanfigueroa;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gafm2
  */
 public class Ex1P1_GermanFigueroa {
-
+    public static Scanner entrada = new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int opcion = 0;
+        do {
+            
+            System.out.println("BIENVENIDO AL MENU");
+            System.out.println("1. Reemplazo de Palabras");
+            System.out.println("2. Vecinos");
+            System.out.println("3. Triforce");
+            System.out.println("4. Bono");
+            System.out.println("0. Salir");
+            System.out.println("Ingrese una opcion: ");
+            opcion = entrada.nextInt();
+             
+
+        } while (opcion != 0);
+    }
+    
+    public static String WordReplace(){
+        System.out.println("Ingrese la cadena: ");
+        String cadena = entrada.next();
+        System.out.println("Ingrese la palabra a buscar: ");
+        String palabra = entrada.next();
+        System.out.println("Ingrese la palabra a reemplazar: ");
+
+        for (int i = 0; i < cadena.length() - 1; i += 1) {
+            char caracter = cadena.charAt(i);
+            int caracterAscii = (int)
+            char acumuladorCadena += acumuladorCadena + 
+        }
+        return "";
+    }
+    public static String Vecinos(){
+        System.out.println("Ingresen una cadena: ");
+        String cadena = entrada.next();
+        
+        int n = entrada.nextInt();
+        System.out.println("Numero generado: " +n);
+    }
+    public static void Triforce(){
+        System.out.println("Ingreso un tamaño impar: ");
+        int numero = entrada.nextInt();
+        int asteriscos = 0;
+        int espacios = (numero/2);
+        for(int j = 0; j <= numero; j += 1 ){
+            for(int i2 = 0; i2 = espacios; i2 += 1){
+        }
+
+    }
+        
+    public static boolean isAnagram(String cadena1, String cadena2){
+        boolean anagramas = false;
+        int contador = 1;
+        int suma1 = 0;
+        int suma2 = 0;
+        if(cadena1.length() == cadena2.length()){
+            for(int i = 0; i <= cadena1.length() -i; i += 1){
+                
+            }
+        }
+        return true;
     }
     
 }
+
+
+
